6692405dcb838f0e7dce3fe0afef5bbf92aee079
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2024-09-14 23:36:17 -0300

    feat: init bootstrap class [#1]

---
+++ docs/js/bootstrap.js
@@ -0,0 +1,83 @@
+/**
+ * @description Bootstrap
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ * @note        For now, this class will work as a wheel builder
+ */
+import Wheel from './core/wheel'
+
+export default class Bootstrap {
+  /**
+   * @type {HTMLCanvasElement}
+   */
+  #canvas
+
+  /**
+   * @type {CanvasRenderingContext2D}
+   */
+  #context
+
+  /**
+   * @type {Wheel}
+   */
+  #wheel
+
+  /**
+   * Constructor
+   *
+   * @param {string}   canvasId
+   * @param {string[]} wheelColors
+   */
+  constructor(canvasId, wheelColors) {
+    this.#canvas = this.#buildCanvas(canvasId)
+    this.#context = this.#canvas.getContext('2d')
+    this.#wheel = this.#buildWheel(wheelColors)
+  }
+
+  /**
+   * Render
+   *
+   * @returns {void}
+   */
+  render() {
+    this.#wheel.update(this.#context)
+  }
+
+  /**
+   * Animate
+   *
+   * @returns {void}
+   */
+  animate() {
+    this.render()
+    requestAnimationFrame(this.animate.bind(this))
+  }
+
+  /**
+   * Build wheel
+   *
+   * @param   {string[]} colors
+   * @returns {Wheel}
+   */
+  #buildWheel(colors) {
+    return new Wheel(
+      this.#canvas.width / 2,
+      this.#canvas.height / 2,
+      this.#canvas.height * 0.4,
+      colors,
+      0.01,
+    )
+  }
+
+  /**
+   * Build canvas
+   *
+   * @param   {string}            canvasId
+   * @returns {HTMLCanvasElement}
+   */
+  #buildCanvas(canvasId) {
+    const canvas = document.getElementById(canvasId)
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    return canvas
+  }
+}
